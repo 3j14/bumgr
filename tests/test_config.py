af0cbc46c3a7fa1d51ae2594333213ac4e638a86
Jonas Drotleff <jonas.drotleff@3j14.de>
2024-11-07 19:54:18 +0100

    Test: Add test for get_config with argument

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,11 +1,18 @@
 from pathlib import Path
+
+import pytest
 from bumgr import get_config
 from tempfile import TemporaryDirectory
 from pytest import MonkeyPatch
 import os
 
 
-def test_get_config(monkeypatch: MonkeyPatch):
+# Note: Other config files, located e.g. at '/etc/bumgr/config.toml'
+# or in the user path (if 'XDG_CONFIG_HOME' is not defined) are hard
+# to test, because it requires accessing and changing the users files.
+
+
+def test_get_config_xdg_config(monkeypatch: MonkeyPatch):
     with TemporaryDirectory() as temp_dir:
         config_dir = Path(temp_dir) / "bumgr"
         config_dir.mkdir(exist_ok=False)
@@ -15,3 +22,13 @@ def test_get_config(monkeypatch: MonkeyPatch):
             m.setitem(os.environ, "XDG_CONFIG_HOME", temp_dir)
             config = get_config()
             assert config == config_file
+
+
+def test_get_config_param():
+    with TemporaryDirectory() as temp_dir:
+        config_file = Path(temp_dir) / "config.toml"
+        with pytest.raises(FileNotFoundError):
+            get_config(str(config_file))
+        config_file.touch(exist_ok=False)
+        config = get_config(str(config_file))
+        assert config == config_file
